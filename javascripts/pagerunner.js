ed4fc1df31acc996f732959e8ec7be6922877548
Max Walters <max.walters10@gmail.com>
2018-07-10 19:52:28 -0400

    Finish initial parseCalc() function

--- javascripts/pagerunner.js
+++ javascripts/pagerunner.js
@@ -53,25 +53,23 @@ function reset(){
 }
 
 // Changes variables for calculation
-function ingame(){
-  document.getElementById("dinner").onclick(puCD += 1);
+document.getElementById("dinner").onclick(puCD += 1);
 
-  document.getElementById("death").onclick(puD += 1);
-  document.getElementById("roofcamp").onclick(cl += 1);
-  document.getElementById("bridgecamp").onclick(br += 1);
-  document.getElementById("bluezone").onclick(el += 1);
+document.getElementById("death").onclick(puD += 1);
+document.getElementById("roofcamp").onclick(cl += 1);
+document.getElementById("bridgecamp").onclick(br += 1);
+document.getElementById("bluezone").onclick(el += 1);
 
-  document.getElementById("redzone").onclick(su += 1);
-  document.getElementById("car").onclick(sup += 1);
-  document.getElementById("pan").onclick(pp += 1);
-  document.getElementById("hitreg").onclick(sq += 1);
+document.getElementById("redzone").onclick(su += 1);
+document.getElementById("car").onclick(sup += 1);
+document.getElementById("pan").onclick(pp += 1);
+document.getElementById("hitreg").onclick(sq += 1);
 
-  document.getElementById("potato").onclick(te += 1);
-  document.getElementById("grenade").onclick(punch += 1);
-  document.getElementById("carflip").onclick(ep += 1);
+document.getElementById("potato").onclick(te += 1);
+document.getElementById("grenade").onclick(punch += 1);
+document.getElementById("carflip").onclick(ep += 1);
 
-  document.getElementById("calculate").onclick(calculate());
-}
+document.getElementById("calculate").onclick(calculate());
 
 // Calculation for workout
 function calculate(){
@@ -88,11 +86,71 @@ function calculate(){
   numOfPP = pp * 15;
   numOfSQ = sq * 5;
   totalPU = numOfPUCD + numOfPUD;
-  var array2parse = [numOfPUCD, numOfPUD, numOfTE, numOfPUNCH, numOfEP, numOfCL, numOfBR, numOfEL, numOfSU, numOfSUP, numOfPP, numOfSQ];
+  // Array indices =    0        1         2          3        4       5         6        7        8         9        10
+  var array2parse = [totalPU, numOfTE, numOfPUNCH, numOfEP, numOfCL, numOfBR, numOfEL, numOfSU, numOfSUP, numOfPP, numOfSQ];
   return parseCalc(array2parse);
 }
 
 // Function to parse array from calc
 function parseCalc(array){
+  var invalidWO = [];
+  var returnString = "";
+  // Check to see which workouts are empty
+  for i in array {
+    if (array[i] == 0) {
+      invalidWO.push(i);
+    }
+  }
+  // Returns list of workouts and reps
+  for j in array {
+    if (invalidWO.contains(j)){
+      continue;
+    } else {
+      switch (j) {
+        case 0: // pushups
+          returnString.concat(array[j] + " pushups, ");
+          break;
+        case 1: // tricep extensions
+          returnString.concat(array[j] + " tricep extensions, ");
+          break;
+        case 2: // punches
+          returnString.concat(array[j] + " punches, ");
+          break;
+        case 3: // elbow plank
+          returnString.concat(getTime(array[j]));
+          break;
+        case 4: // climbers
+          returnString.concat(array[j] + " climbers, ");
+          break;
+        case 5: // bridges
+          returnString.concat(array[j] + " bridges, ");
+          break;
+        case 6: // elbow lifts
+          returnString.concat(array[j] + " elbow lifts, ");
+          break;
+        case 7: // sit ups
+          returnString.concat(array[j] + " sit ups, ");
+          break;
+        case 8: // supermans
+          returnString.concat(array[j] + " supermans, ");
+          break;
+        case 9: // pseudo-planches
+          returnString.concat(array[j] + " pseudo-planches, ");
+          break;
+        case 10: // squats
+          returnString.concat(array[j] + " squats");
+          break;
+        default:
+          returnString = "ERROR. PLEASE CONTACT ADMINISTRATOR.";
+      }
+    }
+  }
+}
 
+function getTime(seconds){
+  var result = seconds/60;
+  var timeString = result.toString();
+  var minutes = timeString.parseInt();
+  var finSeconds = seconds%60;
+  return (minutes + " minutes and " + finSeconds + " seconds elbow plank, ");
 }
